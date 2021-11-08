629b442a4342afcf20489a9290611c065f0e7c26
douryne <53044034+douryne@users.noreply.github.com>
2021-11-08 22:28:51 +0500

    Update app.ts

--- client/app.ts
+++ client/app.ts
@@ -4,8 +4,6 @@ let city = {
     name: 'Tokyo'
 }
 
-const PORT = `${process.env.PORT}`;
-
 async function fetchReq() {
     let weather = await fetch(`http://localhost:${PORT}/getWeather?city=${city.name}`);
     let json = await weather.json()
